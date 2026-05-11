405e485632f7a51fa5b09fde42adfef514019b39
Felix Groß <104097884+grossf@users.noreply.github.com>
2026-05-11 17:26:32 +0200

    Add engine-dependent service unit tests (#19)

--- test/helpers/fake_engine.gd
+++ test/helpers/fake_engine.gd
@@ -3,13 +3,23 @@ extends WeavlyEngine
 const DefaultVariableService = preload(
 	"res://addons/weavly/src/services/implementations/default_variable_service.gd"
 )
+const DefaultStatementService = preload(
+	"res://addons/weavly/src/services/implementations/default_statement_service.gd"
+)
 
 var did_finish: bool = false
+var did_next: bool = false
 
 
 func _init() -> void:
 	variable_service = DefaultVariableService.new()
 	variable_service.initialize(self)
+	statement_service = DefaultStatementService.new()
+	statement_service.initialize(self)
+
+
+func next() -> void:
+	did_next = true
 
 
 func finish() -> void:
