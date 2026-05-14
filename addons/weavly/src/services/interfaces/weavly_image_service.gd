1ee57a447b59e7a2a31e06647003b8697253fec5
Felix Groß <felix-gross@mail.de>
2026-05-14 10:07:09 +0200

    Fix test failure

--- addons/weavly/src/services/interfaces/weavly_image_service.gd
+++ addons/weavly/src/services/interfaces/weavly_image_service.gd
@@ -2,7 +2,7 @@ class_name WeavlyImageService
 extends WeavlyService
 
 
-func get_image(_id: String, _default: Texture2D = null) -> Texture2D:
+func get_image(_id: String, default: Texture2D = null) -> Texture2D:
 	return default
 
 
