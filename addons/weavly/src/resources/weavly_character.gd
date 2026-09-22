15155e5b485453a9a2f948dcd7ceb79bb7484161
Felix Groß <104097884+grossf@users.noreply.github.com>
2026-09-22 15:38:01 +0200

    Reduce WeavlyCharacter to id and display_name (#101)

--- addons/weavly/src/resources/weavly_character.gd
+++ addons/weavly/src/resources/weavly_character.gd
@@ -3,8 +3,3 @@ extends Resource
 
 @export var id: StringName
 @export var display_name: String
-
-@export var primary_color: Color = Color.LIGHT_GRAY
-@export var secondary_color: Color = Color.DIM_GRAY
-
-@export var portrait: Texture2D
